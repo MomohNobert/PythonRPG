0a71da8a882af09a27b1db3b30a1436f590121aa
Momoh Nobert <okhai0112@gmail.com>
2020-06-21 00:02:16 +0100

    added new functions to the person class and made changes to the main

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ magic = [
     {"name": "Fire", "cost": 10, "dmg": 60},
     {"name": "Thunder", "cost": 15, "dmg": 75},
     {"name": "Blizzard", "cost": 20, "dmg": 90},
-    ]
-    
+]
+
 player = Person(460, 65, 60, 34, magic)
+
+print(player.generate_damage())
+print(player.generate_spell_damage(0))
+print(player.generate_spell_damage(1))
