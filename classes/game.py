aa86664a837405c3961cfe6ee44f245b587dc352
Momoh Nobert <okhai0112@gmail.com>
2020-06-22 13:44:06 +0100

    changed health and mp interface to accommodate for empty spaces.

--- classes/game.py
+++ classes/game.py
@@ -102,8 +102,38 @@ class Person:
         while len(mp_bar) < 10:
             mp_bar += " "
 
+        hp_string = str(self.hp) + "/" + str(self.max_hp)
+        current_hp = ""
+
+        if len(hp_string) < 9:
+            decreased = 9 - len(hp_string)
+
+            while decreased > 0:
+                current_hp += " "
+                decreased -= 1
+
+            current_hp += hp_string
+
+        else:
+            current_hp = hp_string
+
+        mp_string = str(self.mp) + "/" + str(self.max_mp)
+        current_mp = ""
+
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+
+            current_mp += mp_string
+
+        else:
+            current_mp = mp_string
+
         print("                       ___________________________             ____________")
         print(bcolors.BOLD + self.name + "      " +
-              str(self.hp) + "/" + str(self.max_hp) + "  " + bcolors.OK_GREEN + "|" + hp_bar + "|    "
-              + str(self.mp) + "/" + str(self.max_mp) + "  " + bcolors.OK_BLUE + "|" + mp_bar + '|')
+              current_hp + "  " + bcolors.OK_GREEN + "|" + hp_bar + "|    "
+              + current_mp + "  " + bcolors.OK_BLUE + "|" + mp_bar + '|')
 
